eef49e215e3ddd8330a99e055e4bfa019ace3e16
Leonardo <leopdor@gmail.com>
2022-03-30 20:21:46 -0300

    Criado Server Java

---
+++ gameServer/src/test/java/br/com/leorocha/gameServer/GameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leorocha.gameServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
